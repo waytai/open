361a1683921cc68c8b6f5522c04a54e6659d2f3e
wayne <278972894@qq.com>
2015-08-20 15:09:09 +0800

    eq

---
+++ java/eq.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: eq.java
+    > Author: wayne
+    > Mail: @163.com 
+    > Created Time: 2015年08月20日 星期四 15时06分38秒
+ ************************************************************************/
+
+public class eq{
+    public static void main(String args[]){
+        String s1 = "java language";
+        String s2 = new String(s1);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s2);
+    }
+}

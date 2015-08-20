926c639bd6104f4c38a1ade27b8470f2168549a3
wayne <278972894@qq.com>
2015-08-20 14:53:16 +0800

    main1

---
+++ java/main1.java
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: hello.java
+    > Author: wayne
+    > Mail: @163.com 
+    > Created Time: 2015年08月20日 星期四 14时43分48秒
+ ************************************************************************/
+
+public class main1{
+    public static void main(String args[]){
+        int i,y;
+        y = 20;
+        for(i=0; i < 10; i++)
+        {
+            System.out.println("this is i:" + i);
+            System.out.println("this is y:" + y);
+            y = y - 1;
+        }
+    }
+}

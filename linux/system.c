28ae977fbc4ec70e98ffa7e3cc075e9184eba05c
wayne <278972894@qq.com>
2015-08-22 22:37:54 +0800

    system

---
+++ linux/system.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: system.c
+    > Author: wayne
+    > Mail: @163.com 
+    > Created Time: 2015年08月22日 星期六 22时35分25秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("running the system\n");
+    system("ps -ax");
+    printf("done\n");
+    exit(0);
+   return 0;
+    }

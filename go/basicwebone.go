d5cd6874d4efe48e36c3bba022f84c3b57fb0e61
wayne <278972894@qq.com>
2017-06-15 15:31:45 +0800

    go web 程序

---
+++ go/basicwebone.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "net/http"
+import "html/template"
+
+func Hello(response http.ResponseWriter, request *http.Request) {
+	type person struct {
+		Id      int
+		Name    string
+		Country string
+	}
+
+	liumiaocn := person{Id: 1001, Name: "liumiaocn", Country: "China"}
+
+	tmpl, err := template.ParseFiles("./test.html")
+	if err != nil {
+		fmt.Println("Error happened..")
+	}
+	tmpl.Execute(response, liumiaocn)
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}

fb78ef0adca6484238c8f2dd2fa407c015785ba8
waytai <liuzhiwei@mxchip.com>
2019-12-19 14:28:41 +0800

    自己写的临接表

--- linux/graph_list.c
+++ linux/graph_list.c
@@ -1,114 +1,117 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <malloc.h>
 #include <string.h>
+#include <malloc.h>
 
-#define MVNum 100
-#define LENGTH(a) (sizeof(a)/sizeof(a[0]))
+#define VNODENUM  100
 
-typedef struct ArcNode{
-    int adjvex;
-    struct ArcNode *nextarc;
-    int info;
-} Acnd, *PAcnd;
+typedef struct arcnode{
+    int vernum;
+    struct arcnode *next_arc;
+} ArcNode, *PArcNode;
 
-typedef struct Vnode {
+typedef struct vnode{
     char data;
-    struct ArcNode *first_edge;
-} Vnd, AdjList[MVNum]; 
-typedef struct ALGraph{
-    AdjList vertices;
-    int vexnum, arcnum;
-} ALGraph, *PALGraph;
+    struct arcnode *first_arc;
+} Vnode, *PVnode;
+
+typedef struct graph{
+    int vernum, arcnum;
+    struct vnode vnode_list[VNODENUM];
+} Graph, *PGraph;
 
 
-int get_position(PALGraph g,char p){
-    int i;
-    for(i=0;i<g->vexnum;i++){
-        if(g->vertices[i].data == p){
+int get_position(PGraph pg, char c){
+    for(int i =0; i< pg->vernum; i++){
+        if(pg->vnode_list[i].data == c){
             return i;
         }
     }
-
     return -1;
 }
 
-int link_last(PAcnd list, PAcnd node){
-    PAcnd p = list;
-    while(p->nextarc)
-        p = p->nextarc;
-    p->nextarc = node;
+
+void link_to_last(PArcNode list, PArcNode pnode){
+   PArcNode p = list;
+   while(p->next_arc) {
+       p= p->next_arc;
+   }
+   p->next_arc = pnode;
 }
 
-
 int main(){
-    char vexs[] = {'A', 'B', 'C','D', 'E', 'F', 'G'};
-    char edge[][2] = {
+    char A[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+    char edge[][2] ={
         {'A', 'C'},
         {'A', 'D'},
         {'A', 'F'},
         {'B', 'C'},
         {'C', 'D'},
         {'E', 'G'},
-        {'F', 'G'}
+        {'G', 'F'}
     };
 
-    int vexnum = LENGTH(vexs);
-    int arcnum = sizeof(edge);
-    printf("%d\n", vexnum);
-    printf("%d\n", arcnum);
+    PGraph pgraph;
+    pgraph = (PGraph)malloc(sizeof(Graph));
+    memset(pgraph, 0, sizeof(Graph));
+    pgraph->vernum = sizeof(A)/sizeof(A[0]);
+    pgraph->arcnum = sizeof(edge)/sizeof(edge[0]);
+    printf("%d    %d\n", pgraph->vernum, pgraph->arcnum);
 
-
-    PALGraph grap; 
-    grap = (ALGraph*)malloc(sizeof(ALGraph));
-    memset(grap,0,sizeof(grap));
-    grap->vexnum = vexnum;
-    grap->arcnum = arcnum;
-    for(int i= 0; i< vexnum; i++){
-        grap->vertices[i].data = vexs[i];
-        grap->vertices[i].first_edge = NULL;
+    for(int i=0; i< pgraph->vernum; i++){
+        pgraph->vnode_list[i].data = A[i];
+        pgraph->vnode_list[i].first_arc = NULL;
     }
 
-    char c1,c2;
-    int p1,p2;
-    PAcnd node1, node2;
-    
-    for(int i=0;i<vexnum; i++){
+    int p1, p2;
+    char c1, c2;
+    printf("-----%d\n", pgraph->arcnum);
+    for(int i=0; i< pgraph->arcnum; i++){
         c1 = edge[i][0];
         c2 = edge[i][1];
-
-        printf("%c,%c\n", c1, c2);
-        p1 = get_position(grap, c1);
-        p2 = get_position(grap, c2);
-        node1 = (PAcnd)malloc(sizeof(Acnd));
-        node1->adjvex = p2;
-        if(grap->vertices[p1].first_edge == NULL)
-            grap->vertices[p1].first_edge = node1;
-        else
-            link_last(grap->vertices[p1].first_edge, node1);
-        //printf("=============\n");
-        //printf("---3333--- %d %d\n", p1, p2);
-
-        node2 = (PAcnd)malloc(sizeof(Acnd));
-        node2->adjvex = p1;
-        if(grap->vertices[p2].first_edge == NULL)
-            grap->vertices[p2].first_edge = node2;
-        else
-            link_last(grap->vertices[p2].first_edge, node2);
-    }
+        printf("%c  %c\n",c1, c2);
+        p1 = get_position(pgraph, c1);
+        p2 = get_position(pgraph, c2);
+        printf("%d   %d\n", p1, p2);
 
 
-    printf("000000--------0000000\n");
-    PAcnd pnode;
+        PArcNode arc_egde1, arc_egde2;
+        arc_egde1 = (PArcNode)malloc(sizeof(ArcNode));
+        arc_egde1->vernum = p2;
 
-    for(int i=0 ;i<grap->vexnum; i++){
-        printf("%c\n", grap->vertices[i].data);
-        pnode  = grap->vertices[i].first_edge;
-        while (pnode != NULL){
-            printf("----%d  %c\n", pnode->adjvex, grap->vertices[pnode->adjvex].data);
-            pnode = pnode->nextarc;
+        if(pgraph->vnode_list[p1].first_arc == NULL)
+        {
+            pgraph->vnode_list[p1].first_arc = arc_egde1;
         }
+        else
+        {
+            link_to_last(pgraph->vnode_list[p1].first_arc, arc_egde1);
+        }
+
+        arc_egde2 = (PArcNode)malloc(sizeof(ArcNode));
+        arc_egde2->vernum = p1;
+        if(pgraph->vnode_list[p2].first_arc == NULL)
+            pgraph->vnode_list[p2].first_arc = arc_egde2;
+        else
+            link_to_last(pgraph->vnode_list[p2].first_arc, arc_egde2);
     }
 
+    printf("the node is\n");
+    for(int i=0;i<pgraph->vernum; i++){
+        printf("%c ", pgraph->vnode_list[i].data);
+    }
+    printf("\n");
+
+    for(int i = 0; i<pgraph->vernum; i++){
+        printf("%c ", pgraph->vnode_list[i].data);
+        PArcNode p_arc;
+        p_arc = pgraph->vnode_list[i].first_arc;
+        while(p_arc != NULL){
+            printf("%d %c  ", p_arc->vernum, pgraph->vnode_list[p_arc->vernum].data);
+            p_arc = p_arc->next_arc;
+        }
+        printf("\n");
+    }
     return 0;
 }
+

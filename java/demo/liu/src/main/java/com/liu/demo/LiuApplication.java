33be6d5e9a4bbf1f1f98d22dab066b3af7db6aff
waytai <278972894@qq.com>
2020-06-06 16:06:48 +0800

    添加spring学习

---
+++ java/demo/liu/src/main/java/com/liu/demo/LiuApplication.java
@@ -0,0 +1,13 @@
+package com.liu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuApplication.class, args);
+    }
+
+}
---
+++ java/demo/liu/src/test/java/com/liu/demo/LiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
